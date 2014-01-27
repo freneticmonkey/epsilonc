32f64f8caeaa3a5a0778b1c8bea5498910f8fa1e
Scott Porter <scottporter@neuroticstudios.com>
2014-01-27 13:52:28 +1100

    M Properly exposed named logging to python

--- source/script/python/LoggingModule.cpp
+++ source/script/python/LoggingModule.cpp
@@ -19,16 +19,14 @@ void initLogging()
 	// This is a virtual base class
 	class_<epsilon::LogListener, epsilon::LogListener::Ptr, boost::noncopyable>("LogListener", no_init);
 
-	/*
-	void ( (epsilon::Logger::*LogNamedContent)(std::string, std::string) ) = &epsilon::Logger::Log;
+	void (*LogNamedContent)(std::string, std::string)= &epsilon::Logger::Log;
 
 	class_<epsilon::Logger, boost::noncopyable>("Logger", no_init)
 		//.def("log", (void (Logger::*)(string, string) ) &Logger::Log, LoggerLog())
-		.def("log", &LogNamedContent)
+		.def("log", LogNamedContent)
 		.staticmethod("log")
 
 		.def("add_listener", &epsilon::Logger::addListener)
 		.def("remove_listener", &epsilon::Logger::removeListener)
 	;
-	*/
 }
