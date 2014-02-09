239391e3ce42523f6f6f76a95beb86f10dbf4046
Scott Porter <scottporter@neuroticstudios.com>
2014-02-09 14:01:08 +1100

    + Added Debug helpers - Trigger VS break point and Debug/Release detection.

--- source/script/python/UtilitiesModule.cpp
+++ source/script/python/UtilitiesModule.cpp
@@ -1,6 +1,38 @@
 #include "script/python/UtilitiesModule.h"
 #include "script/PythonLogListener.h"
 
+
+#ifdef _MSC_VER 
+#include <intrin.h>
+#include <Windows.h>
+#endif
+
+// Expose a method for triggering a break point 
+void TriggerDebugBreak()
+{
+#ifdef _DEBUG
+
+#ifdef _MSC_VER
+	// if visual studio and a debugger is attached, otherwise ignore
+	if (IsDebuggerPresent())
+	{
+		__debugbreak();
+	}
+#endif
+
+#endif
+}
+
+// Expose current compile mode to python
+bool IsDebug()
+{
+	bool debug = false;
+#ifdef _DEBUG
+	debug = true;
+#endif
+	return debug;
+}
+
 void initUtilities()
 {
 
@@ -15,4 +47,7 @@ void initUtilities()
 		.def(init<std::string>())
 		.def("write", &PythonLogListener::OutputLog)
 	;
+
+	def("debug_break", TriggerDebugBreak);
+	def("is_debug", IsDebug);
 }
