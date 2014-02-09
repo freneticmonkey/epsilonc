5c107b2a155a6d7ad5fd38eafda8a3b27764b74d
Scott Porter <scottporter@neuroticstudios.com>
2014-02-09 14:14:45 +1100

    + Added Tilde key to toggle console window. Console window is now hidden by default Added build version display

--- resources/scripts/core/main.py
+++ resources/scripts/core/main.py
@@ -1,6 +1,6 @@
 import sys, os
 
-from epsilon import RenderManager, Input
+from epsilon import RenderManager, Input, UIManager, util
 
 from core.configurationmanager import ConfigurationManager
 
@@ -17,16 +17,28 @@ class ScriptCore(object):
 
 	def on_start(self):
 		print "Starting Main. Initialising Python Managers"
+		if util.is_debug():
+			print "Running Debug Build"
+		else:
+			print "Running Release Build"
 		self._cm = ConfigurationManager.get_instance()
 		self._cm.load_configuration()		
 		
 		self._init_resources()
+
+		# Hide the console window by default
+		self._console_window = UIManager.get_window("console")
+		self._console_window.visible = False
 
 	def on_update(self, dt):
 		
 		# If escape is pressed, close the window
 		if Input.key_down(Input.Key.Escape):
 			RenderManager.close_window()
+
+		# If Tilde is pressed toggle the console window
+		if Input.key_down(Input.Key.Tilde):
+			self._console_window.visible = not self._console_window.visible
 
 	def on_destroy(self):
 		self._cm.save_configuration()
