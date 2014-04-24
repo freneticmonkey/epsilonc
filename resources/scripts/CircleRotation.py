40f3c07c945ceaf120a8ede07675832c9ac1c243
Scott Porter <scottporter@neuroticstudios.com>
2014-04-24 21:19:02 +1000

    M Updated scene to use textures + Added demo texture + Added orbit rotation script for niftier light movement + Position gizmo now focuses lights on 0,0,0 to test spotlights.

---
+++ resources/scripts/CircleRotation.py
@@ -0,0 +1,25 @@
+from epsilon import *
+
+from epsilon.math import Vector3
+
+import math
+
+class CircleRotation(object):
+	
+	def __init__(self):
+		self._active = True
+
+	def on_start(self):
+		pass
+		
+	def on_update(self, dt):
+		if self._active:
+			angle = -dt * math.pi / 2.0
+			self.node.transform.rotate(Vector3.RIGHT, angle)
+
+	def on_destroy(self):
+		pass
+
+# It is necessary to instantiate the class as "_instance"
+# in Python so that epsilon can hook into it.
+_instance = CircleRotation()
--- resources/scripts/PositionGizmo.py
+++ resources/scripts/PositionGizmo.py
@@ -1,5 +1,6 @@
 from epsilon.render import Colour
-from epsilon import Gizmos
+from epsilon import Gizmos 
+from epsilon.math import Vector3
 
 class PositionGizmo(object):
 	
@@ -12,6 +13,8 @@ class PositionGizmo(object):
 	def on_update(self, dt):
 		Gizmos.colour(Colour.YELLOW)
 		Gizmos.draw_sphere(self.node.transform.position, 0.25)
+
+		self.node.transform.lookat(Vector3.ZERO);
 
 	def on_destroy(self):
 		pass
