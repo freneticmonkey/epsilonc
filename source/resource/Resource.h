01f956fedf6dbebcf651b8c19a185f204eb2acc1
Scott Porter <scottporter@neuroticstudios.com>
2014-05-14 23:49:38 +1000

    - Removed unnecessary function.

--- source/resource/Resource.h
+++ source/resource/Resource.h
@@ -41,8 +41,6 @@ namespace epsilon
         // Ensure that type based comparison is implemented?
 //        virtual int operator=;
         
-		long GetReourceId() { return filepath.GetHash();  }
-
 		const OwnerIds & GetOwners() { return ownerIds; }
 		void AddOwner(long owner);
         void RemoveOwner(long owner);
