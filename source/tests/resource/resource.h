b68e3c0c50c53c833e394ca62c3a5771750df793
Scott Porter <scottporter@neuroticstudios.com>
2015-01-27 00:42:20 +1100

    started scriptmanager unit test

--- source/tests/resource/resource.h
+++ source/tests/resource/resource.h
@@ -2,7 +2,15 @@
 
 #include <gtest/gtest.h>
 
+#include <logging/Logging.h>
+
+#include <resource/Resource.h>
+#include <resource/ResourceManager.h>
+
+using namespace epsilon;
+
 TEST(Resource, ResourceManager)
 {
+	ResourceManager * rm = &ResourceManager::GetInstance();
 
 }
